d87247cd4dbe022764ef6fd2e79539f5424614f9
JanoBourian <supermath33@hotmail.com>
2025-04-15 23:49:02 -0600

    add: little format

--- c-programming/tutorial/variables.c
+++ c-programming/tutorial/variables.c
@@ -9,15 +9,18 @@ int main(){
     printf("Sometimes we need double like: %.9lf\n", 37.73);
     printf("And also in-line string like: %s\n", "Hello, my friend");
     printf("Or a string using variables like: %s\n", my_long_text);
+
     // Now is time to talk about sizes
     float number = 19.001;
     double value = 19.000;
     printf("The size of a float number is: %lu\n", sizeof(number));
     printf("The size of a double number is: %lu\n", sizeof(value));
     printf("The size of a string is: %lu\n", sizeof(my_long_text));
+
     // Now is time to talk about explicit convertion
     double divided_by_result = (double) 5/2;
-    printf("The value is %lf and the size is %lu.\n", divided_by_result, sizeof(divided_by_result)); 
+    printf("The value is %lf and the size is %lu.\n", divided_by_result, sizeof(divided_by_result));
+    
     // Information about constants
     const int BIRTHYEAR = 1990;
     printf("This is a constant: %d\n", BIRTHYEAR);
